ff8102318a7ac744019f380558c8dfdcb0211010
Akhtar Ansari <akhtaransari@Akhtars-MacBook-Air.local>
2024-02-08 10:37:14 +0530

    Inbox a Web3 Applicaiton with Smart Contract

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+contract Inbox{
+    string public message;
+    function Inbox (string initMessage) public {
+        message = initMessage;
+    }
+
+    function setMessage (string initMessage) public {
+        message = initMessage;
+    }
+}
